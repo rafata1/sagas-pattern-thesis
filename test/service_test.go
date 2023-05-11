6bc5bc0a02848d9703eb4ea0ea93df3b17273bca
DungCDA <dung.cda@teko.vn>
2023-05-11 20:50:25 +0700

    add integration test case idempotences

--- test/service_test.go
+++ test/service_test.go
@@ -352,3 +352,251 @@ func Test_Case_ExceedCreditLimit(t *testing.T) {
 
 	assert.Equal(t, expectedOrder, actualOrder)
 }
+
+func Test_Full_Flow_Inventory_Idempotence(t *testing.T) {
+	repo := order.NewRepo(getOrderTestingDB())
+
+	// PREPARE TOPICS
+	orderCreatedTopic := getTopicTest(config.DefaultConfig.OrderCreatedTopic)
+	prepareInventoryTopic := getTopicTest(config.DefaultConfig.PrepareInventoryTopic)
+	orderBillTopic := getTopicTest(config.DefaultConfig.OrderBillTopic)
+
+	orderProducer := kafka.NewProducer(config.DefaultConfig.KafkaHost, orderCreatedTopic)
+	billConsumer := kafka.NewConsumer(config.DefaultConfig.KafkaHost, orderBillTopic)
+	orderService := order.NewService(repo, orderProducer, billConsumer, nil)
+	inputOrder := model.Order{
+		CustomerID: 1,
+		ProductID:  2,
+		Amount:     3,
+	}
+	ctx := context.Background()
+	id, err := orderService.CreateOrder(ctx, inputOrder)
+	if err != nil {
+		panic(err)
+	}
+
+	actualOrder, err := repo.GetOrder(ctx, id)
+	if err != nil {
+		panic(err)
+	}
+
+	assert.Equal(t,
+		model.Order{
+			ID:         id,
+			CustomerID: 1,
+			ProductID:  2,
+			Amount:     3,
+			Status:     model.OrderStatusPending,
+			CreatedAt:  actualOrder.CreatedAt,
+			UpdatedAt:  actualOrder.UpdatedAt,
+		},
+		actualOrder,
+	)
+
+	err = orderService.RelayMessage(ctx, 10)
+	if err != nil {
+		panic(err)
+	}
+
+	ordersConsumer := kafka.NewConsumer(config.DefaultConfig.KafkaHost, orderCreatedTopic)
+	inventoryRepo := inventory.NewRepo(getInventoryTestingDB())
+	err = inventoryRepo.CreateInventory(ctx, model.Inventory{
+		ProductID: 2,
+		UnitPrice: 5,
+		Amount:    100,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	inventoryProducer := kafka.NewProducer(config.DefaultConfig.KafkaHost, prepareInventoryTopic)
+	inventoryService := inventory.NewService(inventoryRepo, ordersConsumer, nil, inventoryProducer)
+	// receive message 2 times
+	inventoryService.ConsumeOrders(ctx, 1*time.Second)
+	inventoryService.ConsumeOrders(ctx, 1*time.Second)
+
+	actualInventory, err := inventoryRepo.GetInventory(ctx, 2)
+	if err != nil {
+		panic(err)
+	}
+
+	expectedInventory := model.Inventory{
+		ProductID: 2,
+		UnitPrice: 5,
+		Amount:    97,
+		CreatedAt: actualInventory.CreatedAt,
+		UpdatedAt: actualInventory.UpdatedAt,
+	}
+	assert.Equal(t, expectedInventory, actualInventory)
+	inventoryService.RelayMessage(ctx, 10)
+
+	paymentRepo := payment.NewRepo(getPaymentTestingDB())
+	err = paymentRepo.CreateAccount(ctx, model.Account{
+		CustomerID: 1,
+		Balance:    100,
+	})
+
+	paymentConsumer := kafka.NewConsumer(config.DefaultConfig.KafkaHost, prepareInventoryTopic)
+	paymentProducer := kafka.NewProducer(config.DefaultConfig.KafkaHost, orderBillTopic)
+	paymentService := payment.NewService(paymentRepo, paymentConsumer, paymentProducer)
+	paymentService.ConsumePreparedOrders(ctx, 1*time.Second)
+	paymentService.RelayMessage(ctx, 10)
+
+	actualAccount, err := paymentRepo.GetAccount(ctx, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	expectedAccount := model.Account{
+		CustomerID: 1,
+		Balance:    85,
+		CreatedAt:  actualAccount.CreatedAt,
+		UpdatedAt:  actualAccount.UpdatedAt,
+	}
+
+	assert.Equal(t, expectedAccount, actualAccount)
+
+	orderService.ConsumeBills(ctx, 1*time.Second)
+
+	actualOrder, err = repo.GetOrder(ctx, id)
+	if err != nil {
+		panic(err)
+	}
+
+	expectedOrder := model.Order{
+		ID:         id,
+		CustomerID: 1,
+		ProductID:  2,
+		Amount:     3,
+		Status:     model.OrderStatusBilled,
+		CreatedAt:  actualOrder.CreatedAt,
+		UpdatedAt:  actualOrder.UpdatedAt,
+	}
+
+	assert.Equal(t, expectedOrder, actualOrder)
+}
+
+func Test_Full_Flow_Payment_Idempotence(t *testing.T) {
+	repo := order.NewRepo(getOrderTestingDB())
+
+	// PREPARE TOPICS
+	orderCreatedTopic := getTopicTest(config.DefaultConfig.OrderCreatedTopic)
+	prepareInventoryTopic := getTopicTest(config.DefaultConfig.PrepareInventoryTopic)
+	orderBillTopic := getTopicTest(config.DefaultConfig.OrderBillTopic)
+
+	orderProducer := kafka.NewProducer(config.DefaultConfig.KafkaHost, orderCreatedTopic)
+	billConsumer := kafka.NewConsumer(config.DefaultConfig.KafkaHost, orderBillTopic)
+	orderService := order.NewService(repo, orderProducer, billConsumer, nil)
+	inputOrder := model.Order{
+		CustomerID: 1,
+		ProductID:  2,
+		Amount:     3,
+	}
+	ctx := context.Background()
+	id, err := orderService.CreateOrder(ctx, inputOrder)
+	if err != nil {
+		panic(err)
+	}
+
+	actualOrder, err := repo.GetOrder(ctx, id)
+	if err != nil {
+		panic(err)
+	}
+
+	assert.Equal(t,
+		model.Order{
+			ID:         id,
+			CustomerID: 1,
+			ProductID:  2,
+			Amount:     3,
+			Status:     model.OrderStatusPending,
+			CreatedAt:  actualOrder.CreatedAt,
+			UpdatedAt:  actualOrder.UpdatedAt,
+		},
+		actualOrder,
+	)
+
+	err = orderService.RelayMessage(ctx, 10)
+	if err != nil {
+		panic(err)
+	}
+
+	ordersConsumer := kafka.NewConsumer(config.DefaultConfig.KafkaHost, orderCreatedTopic)
+	inventoryRepo := inventory.NewRepo(getInventoryTestingDB())
+	err = inventoryRepo.CreateInventory(ctx, model.Inventory{
+		ProductID: 2,
+		UnitPrice: 5,
+		Amount:    100,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	inventoryProducer := kafka.NewProducer(config.DefaultConfig.KafkaHost, prepareInventoryTopic)
+	inventoryService := inventory.NewService(inventoryRepo, ordersConsumer, nil, inventoryProducer)
+	// receive message 2 times
+	inventoryService.ConsumeOrders(ctx, 1*time.Second)
+	inventoryService.ConsumeOrders(ctx, 1*time.Second)
+
+	actualInventory, err := inventoryRepo.GetInventory(ctx, 2)
+	if err != nil {
+		panic(err)
+	}
+
+	expectedInventory := model.Inventory{
+		ProductID: 2,
+		UnitPrice: 5,
+		Amount:    97,
+		CreatedAt: actualInventory.CreatedAt,
+		UpdatedAt: actualInventory.UpdatedAt,
+	}
+	assert.Equal(t, expectedInventory, actualInventory)
+	inventoryService.RelayMessage(ctx, 10)
+
+	paymentRepo := payment.NewRepo(getPaymentTestingDB())
+	err = paymentRepo.CreateAccount(ctx, model.Account{
+		CustomerID: 1,
+		Balance:    100,
+	})
+
+	paymentConsumer := kafka.NewConsumer(config.DefaultConfig.KafkaHost, prepareInventoryTopic)
+	paymentProducer := kafka.NewProducer(config.DefaultConfig.KafkaHost, orderBillTopic)
+	paymentService := payment.NewService(paymentRepo, paymentConsumer, paymentProducer)
+	// receive prepared order 2 times
+	paymentService.ConsumePreparedOrders(ctx, 1*time.Second)
+	paymentService.ConsumePreparedOrders(ctx, 1*time.Second)
+	paymentService.RelayMessage(ctx, 10)
+
+	actualAccount, err := paymentRepo.GetAccount(ctx, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	expectedAccount := model.Account{
+		CustomerID: 1,
+		Balance:    85,
+		CreatedAt:  actualAccount.CreatedAt,
+		UpdatedAt:  actualAccount.UpdatedAt,
+	}
+
+	assert.Equal(t, expectedAccount, actualAccount)
+
+	orderService.ConsumeBills(ctx, 1*time.Second)
+
+	actualOrder, err = repo.GetOrder(ctx, id)
+	if err != nil {
+		panic(err)
+	}
+
+	expectedOrder := model.Order{
+		ID:         id,
+		CustomerID: 1,
+		ProductID:  2,
+		Amount:     3,
+		Status:     model.OrderStatusBilled,
+		CreatedAt:  actualOrder.CreatedAt,
+		UpdatedAt:  actualOrder.UpdatedAt,
+	}
+
+	assert.Equal(t, expectedOrder, actualOrder)
+}
